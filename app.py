55bfa6db2a4660c22b955fe983790adae988462e
daoern <noreply@daoern.com>
2022-09-06 04:38:24 +0800

    test

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ api.add_resource(IsProfane, '/is_profane')
 api.add_resource(ProfaneProb, '/profane_prob')
 
 if __name__ == '__main__': 
-    app.run(debug=False, port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
